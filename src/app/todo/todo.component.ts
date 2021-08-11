daac38f04dc4267659c96fb5e9417e744eb95d1b
Khalid El Abbassi <kmettykhalid@gmail.com>
2021-08-11 19:51:42 +0100

    Gérer les données dynamiques

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -12,4 +12,36 @@ export class TodoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  todoOne: string = "Projet 1";
+  todoTow: string = "Projet 2";
+  todoThree: string = "Projet 3";
+  todoFour: string = "Projet 4";
+
+  todos = [
+    {
+      todoName: "Projet 1",
+      todoStatus: false,
+      todoImage: "http://placehold.it/50",
+    },
+    {
+      todoName: "Projet 2",
+      todoStatus: true,
+      todoImage: "http://placehold.it/50",
+    },
+    {
+      todoName: "Projet 3",
+      todoStatus: false,
+      todoImage: "http://placehold.it/50",
+    },
+    {
+      todoName: "Projet 4",
+      todoStatus: false,
+      todoImage: "http://placehold.it/50",
+    }
+  ];
+
+  onChangeStatus(i:number): void {
+    this.todos[i].todoStatus = !this.todos[i].todoStatus;
+  }
+
 }
